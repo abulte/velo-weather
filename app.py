47547369c269e262a564ac121b7d455c50d07cbb
Joachim <joachim.robert@protonmail.com>
2021-11-03 11:24:23 +0100

    Add default fallback for get_locale

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'],
+                                               default='en')
 
 app.jinja_env.globals['get_locale'] = get_locale
 
